e02202daab9cd3a3f38697d1331937239eb1f4ec
Muharrem Servet ANKARALI <msankarali@outlook.com>
2021-01-31 22:06:06 +0300

    asd

---
+++ DataAccess/Concrete/EntityFramework/EfBaseRepositoryDal.cs
@@ -0,0 +1,37 @@
+﻿using DataAccess.Abstract;
+using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfBaseRepositoryDal<T> : IRepositoryDal<T> where T : IEntity
+    {
+        public void Add(T item)
+        {
+
+        }
+
+        public void Delete(T item)
+        {
+
+        }
+
+        public T FindById(int id)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        public List<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T item)
+        {
+
+        }
+    }
+}
