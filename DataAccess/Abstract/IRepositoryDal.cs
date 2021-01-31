54252871dd680fee0a0c0c27725e7ea8d3e874aa
Muharrem Servet ANKARALI <msankarali@outlook.com>
2021-01-31 20:07:54 +0300

    update

---
+++ DataAccess/Abstract/IRepositoryDal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface IRepositoryDal<T>
+    {
+        void Add(T item);
+        void Update(T item);
+        void Delete(T item);
+        List<T> GetAll();
+        T FindById(int id);
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryBaseRepositoryDal.cs
@@ -0,0 +1,41 @@
+﻿using DataAccess.Abstract;
+using Entities.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public abstract class InMemoryBaseRepositoryDal<T> : IRepositoryDal<T> where T : IEntity
+    {
+        List<T> _tList;
+        public InMemoryBaseRepositoryDal()
+        {
+            _tList = new List<T>();
+        }
+        public void Add(T item)
+        {
+            _tList.Add(item);
+        }
+
+        public void Delete(T item)
+        {
+            T itemToDelete = _tList.SingleOrDefault(t => t.Id == item.Id);
+            _tList.Remove(itemToDelete);
+        }
+
+        public T FindById(int id)
+        {
+            return _tList.SingleOrDefault(t => t.Id == id);
+        }
+
+        public List<T> GetAll()
+        {
+            return _tList;
+        }
+
+        public abstract void Update(T item);
+    }
+}
