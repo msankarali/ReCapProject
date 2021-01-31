38f93698d8117006632fa59d86eadb7cc6954001
Muharrem Servet ANKARALI <msankarali@outlook.com>
2021-01-31 18:41:49 +0300

    Entities and Dals added

---
+++ DataAccess/Abstract/ICarDal.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface ICarDal
+    {
+        Car GetById(int id);
+        List<Car> GetAll();
+        void Add();
+        void Update();
+        void Delete();
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,36 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        public void Add()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Car GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
