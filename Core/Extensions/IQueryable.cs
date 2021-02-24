49c14f7556adca6be7daf2f8e4ef608bcc320721
MUHARREM SERVET ANKARALI <mservetankarali@gmail.com>
2021-02-25 00:15:55 +0300

    base

---
+++ Core/Extensions/IQueryable.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Extensions
+{
+    public static class IQueryable
+    {
+        public static IQueryable<T> GetKurumId<T>(this IQueryable<T> queryable, int id)
+            where T : class, IKurumEntity, new()
+        {
+            return queryable.Where(k => k.KurumId == id);
+        }
+    }
+}
